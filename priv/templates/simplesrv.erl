a0290bb6871a295fb91580099fee28c107e60dd6
Geoff Cant <nem@erlang.geek.nz>
2011-09-14 16:05:49 -0700

    simplesrv improvements: * log unknown calls * don't respond to unknown calls.

--- priv/templates/simplesrv.erl
+++ priv/templates/simplesrv.erl
@@ -1,50 +1,69 @@
+%%%-------------------------------------------------------------------
+%% @copyright Geoff Cant
+%% @author Geoff Cant <nem@erlang.geek.nz>
+%% @version {@vsn}, {@date} {@time}
+%% @doc
+%% @end
+%%%-------------------------------------------------------------------
 -module({{srvid}}).
+
 -behaviour(gen_server).
--define(SERVER, ?MODULE).
 
-%% ------------------------------------------------------------------
-%% API Function Exports
-%% ------------------------------------------------------------------
+-include("{{appid}}_log.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
--export([start_link/0]).
+%% API
+-export([start_link/0
+        ]).
 
-%% ------------------------------------------------------------------
-%% gen_server Function Exports
-%% ------------------------------------------------------------------
-
+%% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
-%% ------------------------------------------------------------------
-%% API Function Definitions
-%% ------------------------------------------------------------------
+-record(state, {replaceme}).
 
+%%====================================================================
+%% API
+%%====================================================================
+%%--------------------------------------------------------------------
+%% @spec start_link() -> {ok,Pid} | ignore | {error,Error}
+%% @doc Starts the server
+%% @end
+%%--------------------------------------------------------------------
 start_link() ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+    gen_server:start_link(?MODULE, [], []).
 
-%% ------------------------------------------------------------------
-%% gen_server Function Definitions
-%% ------------------------------------------------------------------
+%%====================================================================
+%% gen_server callbacks
+%%====================================================================
 
-init(Args) ->
-    {ok, Args}.
+%% @private
+init([]) ->
+    {ok, #state{}}.
 
-handle_call(_Request, _From, State) ->
-    {noreply, ok, State}.
-
-handle_cast(_Msg, State) ->
+%% @private
+handle_call(Call, _From, State) ->
+    ?WARN("Unexpected call ~p.", [Call]),
     {noreply, State}.
 
-handle_info(_Info, State) ->
+%% @private
+handle_cast(Msg, State) ->
+    ?WARN("Unexpected cast ~p", [Msg]),
     {noreply, State}.
 
+%% @private
+handle_info(Info, State) ->
+    ?WARN("Unexpected info ~p", [Info]),
+    {noreply, State}.
+
+%% @private
 terminate(_Reason, _State) ->
     ok.
 
+%% @private
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
-%% ------------------------------------------------------------------
-%% Internal Function Definitions
-%% ------------------------------------------------------------------
-
+%%--------------------------------------------------------------------
+%%% Internal functions
+%%--------------------------------------------------------------------
