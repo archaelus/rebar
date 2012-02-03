cccbed3e7d3faa0fd2249b12e773e2e33f2d2a86
Geoff Cant <nem@erlang.geek.nz>
2012-02-02 22:07:30 -0800

    Adding my version of the simplefsm template.

--- priv/templates/simplefsm.erl
+++ priv/templates/simplefsm.erl
@@ -1,6 +1,19 @@
+%%%-------------------------------------------------------------------
+%% @copyright Geoff Cant
+%% @author Geoff Cant <nem@erlang.geek.nz>
+%% @version {@vsn}, {@date} {@time}
+%% @doc
+%% @end
+%%%-------------------------------------------------------------------
+
 -module({{fsmid}}).
 -behaviour(gen_fsm).
 -define(SERVER, ?MODULE).
+
+-include("{{appid}}_log.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+-record(state, {replaceme}).
 
 %% ------------------------------------------------------------------
 %% API Function Exports
@@ -21,37 +34,49 @@
 %% ------------------------------------------------------------------
 
 start_link() ->
-    gen_fsm:start_link({local, ?SERVER}, ?MODULE, [], []).
+    gen_fsm:start_link(?MODULE, [], []).
 
 %% ------------------------------------------------------------------
 %% gen_fsm Function Definitions
 %% ------------------------------------------------------------------
 
+%% @private
 init(_Args) ->
-    {ok, initial_state_name, initial_state}.
+    {ok, initial_state_name, #state{}}.
 
+%% @private
 state_name(_Event, State) ->
     {next_state, state_name, State}.
 
-state_name(_Event, _From, State) ->
-    {reply, ok, state_name, State}.
+%% @private
+state_name(Event, _From, State) ->
+    ?WARN("[state ~p] Unexpected event ~p",
+          [state_name, Event]),
+    {next_state, state_name, State}.
 
+%% @private
 handle_event(_Event, StateName, State) ->
     {next_state, StateName, State}.
 
-handle_sync_event(_Event, _From, StateName, State) ->
-    {reply, ok, StateName, State}.
-
-handle_info(_Info, StateName, State) ->
+%% @private
+handle_sync_event(Event, _From, StateName, State) ->
+    ?WARN("[state ~p] Unexpected event ~p",
+          [StateName, Event]),
     {next_state, StateName, State}.
 
+%% @private
+handle_info(Info, StateName, State) ->
+    ?WARN("Unexpected info ~p", [Info]),
+    {next_state, StateName, State}.
+
+%% @private
 terminate(_Reason, _StateName, _State) ->
     ok.
 
+%% @private
 code_change(_OldVsn, StateName, State, _Extra) ->
     {ok, StateName, State}.
 
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
-
